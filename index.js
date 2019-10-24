98c9eadbdcb3a0db7dbaeb175aeda1d12278f9e2
mhasnain <md.hasnain@dfki.de>
2019-10-24 18:08:32 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const name='Mosh';
+
+let persion = {
+    name:'Hasnain',
+    age: 30
+};
+
+let selectedColor = ['red', 'blue'];
+selectedColor[2] = 1;
+
+function greet(name, last){
+    console.log('Hellossss  ' + name + ' ' + last);
+}
+
+function squer(number){
+    return number * number;
+}
 
 
-console.log(name);
+//console.
+
+console.log(selectedColor);
+greet('jone', 'stock');
+greet('Hasnain');
+
+console.log(squer(3));
+
+
